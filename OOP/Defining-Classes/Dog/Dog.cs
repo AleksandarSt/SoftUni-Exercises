fcf814ea98c88797ed3d7515043872294a791df5
AleksandarSt <A_V_S_@abv.bg>
2015-12-20 23:57:56 +0200

    Defining classes added

---
+++ OOP/Defining-Classes/Dog/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Dog
+{
+    class Dog
+    {
+        public Dog()
+            :this(null,null)
+        {
+
+        }
+
+        public Dog(string name,string breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine("{0} is {1} and barks: WOW-WOW!",this.Name??"Unnamed dog",this.Breed??"Unknown breed");
+        }
+    }
+}
---
+++ OOP/Defining-Classes/Dog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dog unnamedDog = new Dog();
+            Dog unnamedSmallDog = new Dog();
+            Dog someDog = new Dog("Lassy", "Pekinez");
+
+            unnamedDog.Breed = "Doberman";
+
+            unnamedDog.Bark();
+            unnamedSmallDog.Bark();
+            someDog.Bark();
+        }
+    }
+}

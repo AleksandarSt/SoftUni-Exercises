2f3032b1a327fb0021d7b74b6440708faa1fbf99
AleksandarSt <A_V_S_@abv.bg>
2015-12-26 22:05:06 +0200

    Exception Handling excersise

---
+++ OOP/Exception-Handling/Exception-Handling/MainProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class MainProgram
+    {
+        public static void Main()
+        {
+            try
+            {
+                Person pesho = new Person("Pesho", "Peshev", 24);
+
+                //Person gosho = new Person("Gosho", string.Empty, 40);
+                //Person misho = new Person(string.Empty, "Mishev", 43);
+                Person paco = new Person("Ivan", "Ivanov", 121);
+            }
+            catch (ArgumentNullException ex)
+            {
+
+                Console.WriteLine("Execption thrown: {0}", ex.Message);
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Execption thrown: {0}", ex.Message);
+            }
+        }
+
+        
+
+    }
+}
---
+++ OOP/Exception-Handling/Exception-Handling/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName 
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Invalid first name!");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Invalid last name!");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age 
+        {
+            get { return this.age; }
+            set
+            {
+                if (value<0||value>120)
+                {
+                    throw new ArgumentOutOfRangeException("Age should be between 0 and 120 years!");
+                }
+            }
+        }
+    }
+}

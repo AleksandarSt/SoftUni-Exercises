84fc8fa81a5582a3f6d90e6e9dbb78778643a4d3
AleksandarSt <A_V_S_@abv.bg>
2016-01-02 00:49:37 +0200

    Extension methods exercise added

---
+++ Advanced-C#/Functional-Programing/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+namespace ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        public static string Substring(this StringBuilder target, int startIndex, int count)
+        {
+            StringBuilder substring= new StringBuilder();
+
+            for (int i = 0 ; i < count; i++)
+            {
+                substring.Append(target[startIndex]);
+                startIndex++;
+            }
+
+            return substring.ToString();
+        }
+    }
+}
---
+++ Advanced-C#/Functional-Programing/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder alphabet = new StringBuilder();
+
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                alphabet.Append((char)i);
+            }
+
+
+            string firstTenLetters = alphabet.Substring(0, 10);
+
+            Console.WriteLine(firstTenLetters);
+        }
+    }
+}

375a72e953ba5de471e28d762c7cc97a1fd73330
AleksandarSt <A_V_S_@abv.bg>
2016-01-17 20:38:49 +0200

    Selection sort algorithm added

---
+++ High-Quality-Code/Defensive-Programming/Well-Known-Algorithm/SelectionSortAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Well_Known_Algorithm
+{
+    class SelectionSortAlgorithm
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {8, 5, 2, 6, 9, 3, 10, 7, 1, 0};
+
+                SelectionSort(numbers);
+        }
+
+        static void SelectionSort(int[] numbers)
+        {
+            for (int currentIndex = 0; currentIndex < numbers.Length-1; currentIndex++)
+            {
+
+                int minIndex = currentIndex;
+                for (int nextIndex = currentIndex + 1; nextIndex < numbers.Length; nextIndex++)
+                {
+                    if (numbers[nextIndex]<numbers[minIndex])
+                    {
+                        minIndex = nextIndex;
+                    }
+                }
+
+                if (minIndex!=currentIndex)
+                {
+                    Swap(numbers, minIndex, currentIndex);
+                }
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                Debug.Assert(numbers[i] < numbers[i + 1], "Sorting is not working correctly!");
+            }
+        }
+
+        private static void Swap(int[] numbers, int minIndex, int currentIndex)
+        {
+            int oldValue = numbers[minIndex];
+            numbers[minIndex] = numbers[currentIndex];
+            numbers[currentIndex] = oldValue;
+        }
+    }
+}
